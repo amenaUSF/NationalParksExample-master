2a5267f6c960a1b7ea9136ad0481315a3a3ee993
Amena Shahid <amenashahid@usf.edu>
2020-03-31 21:53:33 -0400

    Add project files.

---
+++ NationalParks/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NationalParks.Models;
+using NationalParks.APIHandlerManager;
+using Newtonsoft.Json;
+
+namespace NationalParks.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Parks()
+        {
+            Console.WriteLine("in the parks action");
+            APIHandler webHandler = new APIHandler();
+            Console.WriteLine("after the apihandler");
+
+            Parks parks = webHandler.GetParks()[0];
+
+            return View(parks);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NationalParks/Models/Parks.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NationalParks.Models
+{
+  // Model for parks data. These classes can be obtained by either using 
+  // the Visual Studio editor (right-click -> Paste Special -> Paste Json as Classes)
+  // or sites such as JsonToCSHarp
+  public class detail
+  {
+        public string objective { get; set; }
+        public string title { get; set; }
+        public string classification { get; set; }
+        public string specification { get; set; }
+    }
+
+    public class Parks
+  {
+        public string classification { get; set; }
+        public string title { get; set; }
+        public string section { get; set; }
+        public string topic { get; set; }
+        public string subtopic { get; set; }
+        public string desired_outcome { get; set; }
+        public List<detail> details { get; set; }
+  }
+}
